8eb618820f75f4a50e0d7eaf6f0700525f2c318e
Samuel Adebayo <soluadebayo@gmail.com>
2025-05-12 16:19:22 +0100

    updated code

---
+++ benchmark/utils.py
@@ -0,0 +1,22 @@
+import os
+
+def get_badSegment(folder_directory):
+    """
+    This function gets the filenames of all the bad segments in the given directory,
+    Then saves them into a text file called bad_segments.txt.
+    :param folder_directory:
+    :return:
+    """
+    bad_segments = []
+    for bad_files in os.listdir(folder_directory):
+        if bad_files.lower().endswith('mp4'):
+            bad_segments.append(bad_files)
+    with open('bad_segments.txt', 'w') as f:
+        for bad_file in bad_segments:
+            f.write(bad_file + '\n')
+    print(f"Bad segments saved to bad_segments.txt")
+
+if __name__ == '__main__':
+    folder_directory = '/home/samuel/Downloads/Bad_Segments'
+    get_badSegment(folder_directory)
+
